5be3e0b26237057c7e9f8347a9ba6228ae7bb02c
Guilherme <flashnat.br@gmail.com>
2022-12-17 16:57:50 -0300

    React adicionado - configurações iniciais

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        <h1>Game Merger</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
