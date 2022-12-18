9942220ee4b4d70c901869781a3d1fc3864e2b8a
Guilherme <flashnat.br@gmail.com>
2022-12-18 20:47:39 -0300

    teste de chave da API

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,8 @@ import SearchBar from '../components/SearchBar';
 import TitleText from '../components/TitleText';
 import Header from '../components/Header';
 
+import GetRawgKey from '../GetRawgKey';
+console.log(GetRawgKey())
 
 function Main() {
   return (
