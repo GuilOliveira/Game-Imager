572d9042a7ca6385aa66fbb6b7262b34112e568a
Guilherme <flashnat.br@gmail.com>
2022-12-19 04:25:41 -0300

    Feature Add: Busca de jogos com Suggestions

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
 import './SearchBar.css';
 
-const SearchBar = () => {
+
+
+const SearchBar = ({actualText,selectedGames, setSelectedGames, 
+                    setActualText, setSearchedText,
+                    searchResponse, setSearchResponse}) => {
+
+    const clearData = () =>{
+        setSearchResponse({results:[""]})
+    }
+
+    const modifie = (text) =>{
+        clearData()
+        setActualText(text.target.value)
+    }
+
+    const enter = () =>{
+        clearData()
+        setSearchedText(actualText)
+    }
+
     return (
-        <div className='search'>
-            <input type='text' className='searchBar'/>
-            <Button className='searchButton'>🔍</Button>
+        <div >
+            <div className='search'>
+                <input type='text' className='searchBar'
+                value={actualText} onChange={(e) => modifie(e)}
+                onKeyPress={event => {if (event.key === 'Enter') {enter()}}}
+                />
+                <button className='searchButton' onClick={()=>enter()} >🔍</button>
+            </div>
+            {searchResponse.results[0] != "" && (
+        <div className="dataResult">
+          {searchResponse.results.slice(0, 15).map((value) => {
+            return (
+              <a className="dataItem" target="_blank">
+                <p className='gameOptions'>{value.name}</p>
+              </a>
+            );
+          })}
+        </div>
+      )}
         </div>
     );
   }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,16 +4,50 @@ import TitleText from '../components/TitleText';
 import Header from '../components/Header';
 
 import GetRawgKey from '../GetRawgKey';
-console.log(GetRawgKey())
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const API_KEY = GetRawgKey()
+const URL = "https://api.rawg.io/api/games?"
+
 
 function Main() {
+
+  const [actualText, setActualText] = useState();
+  const [searchedText, setSearchedText] = useState();
+  const [searchResponse, setSearchResponse] = useState({results:[""]});
+  const [selectedGames, setSelectedGames] = useState();
+
+  useEffect(() =>  {
+    async function fetchApi (){
+      if (searchedText == undefined){return}
+      if (searchedText == ""){return}
+      const response = await fetch(`${URL}search=${encodeURI(searchedText)}&key=${API_KEY}`)
+          const responsejson = await response.json()
+          console.log(responsejson)
+          setSearchResponse(responsejson)
+      }
+    fetchApi()
+  }, [searchedText]);
+
+
+
   return (
     <div className="Main">
       <header className="Main-header">
         <Header/>
       </header>
       <TitleText/>
-      <SearchBar/>
+      <SearchBar actualText={actualText} 
+        searchedText={searchedText} 
+        setActualText={setActualText} 
+        setSearchedText={setSearchedText}
+        searchResponse={searchResponse}
+        setSearchResponse={setSearchResponse}
+        selectedGames={selectedGames}
+        setSelectedGames={selectedGames}/>
+      
     </div>
   );
 }
